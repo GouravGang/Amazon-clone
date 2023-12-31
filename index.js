658929e246950bc603a801092ac2af865c190741
GouravGang <140442865+GouravGang@users.noreply.github.com>
2024-01-01 01:14:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// document.querySelector(".panel-content").classList.toggle("some");
+// document.querySelector(".panel-content").style.backgroundColor="red";
+// document.querySelector(".panel-content").style.border="2px solid black";
+// document.querySelector(".panel-content p").textcontent="<em>GB</em>";
+
+// document.querySelector(".panel-content p").style.textShadow = "5px 5px 1px #ff0000";
+
+document.querySelector(".left-arrow").addEventListener("click",function(){
+     alert("Hello World!"); 
+    })
+document.querySelector(".right-arrow").addEventListener("click",function(){ 
+    alert("Hello World!"); 
+})
